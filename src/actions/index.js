1be2b30b3d6f872815e36390d3003e9be7043e1d
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2023-02-06 19:37:38 +0500

    add actions of redux and make reducer files

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+export const addTodo = () => {
+  return {
+    type: "ADD_TODO",
+  };
+};
+export const deleteTodo = () => {
+  return {
+    type: "DELETE_TODO",
+  };
+};
+export const clearAllTodo = () => {
+  return {
+    type: "CLEAR_TODO",
+  };
+};
